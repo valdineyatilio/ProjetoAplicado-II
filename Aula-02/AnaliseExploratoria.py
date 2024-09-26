1984ff5bce8f02fd5c011fc830abe6568ef2767f
Valdiney Atílio Pedro <valdineyatiliopedro@gmail.com>
2024-09-26 10:32:09 -0300

    To update arq py

--- Aula-02/AnaliseExploratoria.py
+++ Aula-02/AnaliseExploratoria.py
@@ -1,12 +1,13 @@
+#Bibliotecas python
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Carregar os dados do arquivo CSV com a codificação 'latin1'
+# Carregar a base de dados do arquivo CSV
 url = "https://github.com/valdineyatilio/ProjetoAplicado-II/raw/refs/heads/main/Aula-02/BaseDeDadosYouTube.csv"
 dados_youtube = pd.read_csv(url, encoding='latin1')
 
-# Visualizar as primeiras linhas do DataFrame
+# Vizualição das primeiras linhas do data frame
 print(dados_youtube.head())
 
 # Verificar informações sobre as colunas e tipos de dados
